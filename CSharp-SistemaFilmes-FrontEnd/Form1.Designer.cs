1474ecaeb69bdd39faf6dd9cf10b07ca477d8efb
vinicius viana <vinicius.soares@aedb.br>
2021-01-29 10:52:46 -0300

    Add project files.

---
+++ CSharp-SistemaFilmes-FrontEnd/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace CSharp_SistemaFilmes_FrontEnd {
+    partial class frmTela {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTela));
+            this.pnSuperior = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnCalendario = new System.Windows.Forms.Button();
+            this.btnBiblioteca = new System.Windows.Forms.Button();
+            this.btnLancamentos = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.PnPrincipal = new System.Windows.Forms.Panel();
+            this.userControl21 = new CSharp_SistemaFilmes_FrontEnd.UserControl2();
+            this.userControl11 = new CSharp_SistemaFilmes_FrontEnd.userControl1();
+            this.userControl31 = new CSharp_SistemaFilmes_FrontEnd.UserControl3();
+            this.pnSuperior.SuspendLayout();
+            this.PnPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnSuperior
+            // 
+            this.pnSuperior.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(73)))), ((int)(((byte)(125)))));
+            this.pnSuperior.Controls.Add(this.textBox1);
+            this.pnSuperior.Controls.Add(this.button6);
+            this.pnSuperior.Controls.Add(this.button5);
+            this.pnSuperior.Controls.Add(this.button4);
+            this.pnSuperior.Controls.Add(this.btnCalendario);
+            this.pnSuperior.Controls.Add(this.btnBiblioteca);
+            this.pnSuperior.Controls.Add(this.btnLancamentos);
+            this.pnSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnSuperior.Location = new System.Drawing.Point(0, 0);
+            this.pnSuperior.Name = "pnSuperior";
+            this.pnSuperior.Size = new System.Drawing.Size(1300, 50);
+            this.pnSuperior.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(94)))), ((int)(((byte)(155)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(787, 0);
+            this.textBox1.MaximumSize = new System.Drawing.Size(500, 500);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 50);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(81)))), ((int)(((byte)(143)))));
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(1052, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(53, 50);
+            this.button6.TabIndex = 1;
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(1226, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(31, 49);
+            this.button5.TabIndex = 1;
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(1260, 1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 49);
+            this.button4.TabIndex = 1;
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnCalendario
+            // 
+            this.btnCalendario.FlatAppearance.BorderSize = 0;
+            this.btnCalendario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalendario.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalendario.ForeColor = System.Drawing.Color.White;
+            this.btnCalendario.Image = ((System.Drawing.Image)(resources.GetObject("btnCalendario.Image")));
+            this.btnCalendario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalendario.Location = new System.Drawing.Point(455, 0);
+            this.btnCalendario.Name = "btnCalendario";
+            this.btnCalendario.Size = new System.Drawing.Size(227, 50);
+            this.btnCalendario.TabIndex = 1;
+            this.btnCalendario.Text = "Calendário";
+            this.btnCalendario.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalendario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCalendario.UseVisualStyleBackColor = true;
+            this.btnCalendario.Click += new System.EventHandler(this.btnCalendario_Click);
+            // 
+            // btnBiblioteca
+            // 
+            this.btnBiblioteca.FlatAppearance.BorderSize = 0;
+            this.btnBiblioteca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBiblioteca.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBiblioteca.ForeColor = System.Drawing.Color.White;
+            this.btnBiblioteca.Image = ((System.Drawing.Image)(resources.GetObject("btnBiblioteca.Image")));
+            this.btnBiblioteca.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBiblioteca.Location = new System.Drawing.Point(229, 0);
+            this.btnBiblioteca.Name = "btnBiblioteca";
+            this.btnBiblioteca.Size = new System.Drawing.Size(227, 50);
+            this.btnBiblioteca.TabIndex = 1;
+            this.btnBiblioteca.Text = "Biblioteca";
+            this.btnBiblioteca.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBiblioteca.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBiblioteca.UseVisualStyleBackColor = true;
+            this.btnBiblioteca.Click += new System.EventHandler(this.btnBiblioteca_Click);
+            // 
+            // btnLancamentos
+            // 
+            this.btnLancamentos.FlatAppearance.BorderSize = 0;
+            this.btnLancamentos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLancamentos.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLancamentos.ForeColor = System.Drawing.Color.White;
+            this.btnLancamentos.Image = ((System.Drawing.Image)(resources.GetObject("btnLancamentos.Image")));
+            this.btnLancamentos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLancamentos.Location = new System.Drawing.Point(0, 0);
+            this.btnLancamentos.Name = "btnLancamentos";
+            this.btnLancamentos.Size = new System.Drawing.Size(229, 50);
+            this.btnLancamentos.TabIndex = 1;
+            this.btnLancamentos.Text = "Lançamentos";
+            this.btnLancamentos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLancamentos.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLancamentos.UseVisualStyleBackColor = true;
+            this.btnLancamentos.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PnPrincipal
+            // 
+            this.PnPrincipal.Controls.Add(this.userControl31);
+            this.PnPrincipal.Controls.Add(this.userControl21);
+            this.PnPrincipal.Controls.Add(this.userControl11);
+            this.PnPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnPrincipal.Location = new System.Drawing.Point(0, 50);
+            this.PnPrincipal.Name = "PnPrincipal";
+            this.PnPrincipal.Size = new System.Drawing.Size(1300, 660);
+            this.PnPrincipal.TabIndex = 1;
+            this.PnPrincipal.Paint += new System.Windows.Forms.PaintEventHandler(this.PnPrincipal_Paint);
+            // 
+            // userControl21
+            // 
+            this.userControl21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(31)))), ((int)(((byte)(50)))));
+            this.userControl21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.userControl21.Location = new System.Drawing.Point(0, 0);
+            this.userControl21.Name = "userControl21";
+            this.userControl21.Size = new System.Drawing.Size(1300, 660);
+            this.userControl21.TabIndex = 1;
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(31)))), ((int)(((byte)(50)))));
+            this.userControl11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.userControl11.Location = new System.Drawing.Point(0, 0);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(1300, 660);
+            this.userControl11.TabIndex = 0;
+            // 
+            // userControl31
+            // 
+            this.userControl31.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(31)))), ((int)(((byte)(50)))));
+            this.userControl31.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.userControl31.Location = new System.Drawing.Point(0, 0);
+            this.userControl31.Name = "userControl31";
+            this.userControl31.Size = new System.Drawing.Size(1300, 660);
+            this.userControl31.TabIndex = 0;
+            // 
+            // frmTela
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(31)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(1300, 710);
+            this.Controls.Add(this.PnPrincipal);
+            this.Controls.Add(this.pnSuperior);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmTela";
+            this.Text = "Form1";
+            this.pnSuperior.ResumeLayout(false);
+            this.pnSuperior.PerformLayout();
+            this.PnPrincipal.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnSuperior;
+        private System.Windows.Forms.Button btnLancamentos;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button btnCalendario;
+        private System.Windows.Forms.Button btnBiblioteca;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel PnPrincipal;
+        private userControl1 userControl11;
+        private UserControl2 userControl21;
+        private UserControl3 userControl31;
+    }
+}
+
---
+++ CSharp-SistemaFilmes-FrontEnd/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_SistemaFilmes_FrontEnd {
+    public partial class frmTela : Form {
+        public frmTela() {
+            InitializeComponent();
+            btnLancamentos.BackColor = PnPrincipal.BackColor;
+            userControl11.BringToFront();         
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            textBox1.Text = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            btnLancamentos.BackColor = PnPrincipal.BackColor;
+            btnCalendario.BackColor = pnSuperior.BackColor;
+            btnBiblioteca.BackColor = pnSuperior.BackColor;
+            userControl11.BringToFront();
+        }
+
+        private void PnPrincipal_Paint(object sender, PaintEventArgs e) {
+
+        }
+
+        private void btnBiblioteca_Click(object sender, EventArgs e) {
+            btnBiblioteca.BackColor = PnPrincipal.BackColor;
+            btnCalendario.BackColor = pnSuperior.BackColor;
+            btnLancamentos.BackColor = pnSuperior.BackColor;
+            userControl21.BringToFront();
+        }
+
+        private void btnCalendario_Click(object sender, EventArgs e) {
+            btnCalendario.BackColor = PnPrincipal.BackColor;
+            btnBiblioteca.BackColor = pnSuperior.BackColor;
+            btnLancamentos.BackColor = pnSuperior.BackColor;
+            userControl31.BringToFront();
+        }
+    }
+}
